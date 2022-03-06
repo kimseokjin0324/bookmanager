44cebf2c5179637995b987abaa19bcacd5238ee0
kimseokjin <kimseokjin0324@gmail.com>
2022-03-06 16:18:38 +0900

    JPARepository 의 메소드 findall 정렬(DESC)테스트

--- src/test/java/com/example/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/example/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -4,8 +4,9 @@ import com.example.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -15,11 +16,8 @@ class UserRepositoryTest {
 
     @Test
     void crud() {   //C: CREATE R: READ U: UPDATE D: DELETE
-
-
-        userRepository.save(new User());
-
-        userRepository.findAll().forEach(System.out::println);
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+        users.forEach(System.out::println);
 
 
     }
