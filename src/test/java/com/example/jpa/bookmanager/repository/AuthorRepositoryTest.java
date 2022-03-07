b8edce1d25fe79728b9817451aab0abc9cf084e8
kimseokjin <kimseokjin0324@gmail.com>
2022-03-07 21:37:37 +0900

    M:N인 연관관계를 중간 테이블을 만들어서 1:N으로 변경해서 테스트 진행을 하였다.

--- src/test/java/com/example/jpa/bookmanager/repository/AuthorRepositoryTest.java
+++ src/test/java/com/example/jpa/bookmanager/repository/AuthorRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.jpa.bookmanager.repository;
 
 import com.example.jpa.bookmanager.domain.Author;
 import com.example.jpa.bookmanager.domain.Book;
+import com.example.jpa.bookmanager.domain.BookAndAuthor;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,8 @@ class AuthorRepositoryTest {
     private AuthorRepository authorRepository;
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private BookAndAuthorRepository bookAndAuthorRepository;
 
     @Test
     @Transactional
@@ -28,20 +31,34 @@ class AuthorRepositoryTest {
 
         Author author1 = givenAuthor("김석진");
         Author author2 = givenAuthor("이석진");
+//        book1.addAuthor(author1);
+//        book2.addAuthor(author2);
+//        book3.addAuthor(author1, author2);
+//        book4.addAuthor(author1, author2);
+//
+//        author1.addBook(book1, book3, book4);
+//        author2.addBook(book2, book3, book4);
 
-        book1.addAuthor(author1);
-        book2.addAuthor(author2);
-        book3.addAuthor(author1, author2);
-        book4.addAuthor(author1, author2);
+        BookAndAuthor bookAndAuthor1 = givenBookAndAuthor(book1, author1);
+        BookAndAuthor bookAndAuthor2 = givenBookAndAuthor(book2, author2);
+        BookAndAuthor bookAndAuthor3 = givenBookAndAuthor(book3, author1);
+        BookAndAuthor bookAndAuthor4 = givenBookAndAuthor(book3, author2);
+        BookAndAuthor bookAndAuthor5 = givenBookAndAuthor(book4, author1);
+        BookAndAuthor bookAndAuthor6 = givenBookAndAuthor(book4, author2);
 
-        author1.addBook(book1, book3, book4);
-        author2.addBook(book2, book3, book4);
+        book1.addBookAndAuthors(bookAndAuthor1);
+        book2.addBookAndAuthors(bookAndAuthor2);
+        book3.addBookAndAuthors(bookAndAuthor3, bookAndAuthor4);
+        book4.addBookAndAuthors(bookAndAuthor5, bookAndAuthor6);
 
-        bookRepository.saveAll(Lists.newArrayList(book1,book2,book3,book4));
-        authorRepository.saveAll(Lists.newArrayList(author1,author2));
+        author1.addBookAndAuthors(bookAndAuthor1, bookAndAuthor3, bookAndAuthor5);
+        author2.addBookAndAuthors(bookAndAuthor2, bookAndAuthor4, bookAndAuthor6);
 
-        System.out.println("authors through book : "+bookRepository.findAll().get(2).getAuthors());
-        System.out.println("books through author : "+authorRepository.findAll().get(0).getBooks());
+        bookRepository.saveAll(Lists.newArrayList(book1, book2, book3, book4));
+        authorRepository.saveAll(Lists.newArrayList(author1, author2));
+
+        bookRepository.findAll().get(2).getBookAndAuthors().forEach(o -> System.out.println(o.getAuthor()));
+        authorRepository.findAll().get(0).getBookAndAuthors().forEach(o -> System.out.println(o.getBook()));
     }
 
     private Book givenBook(String name) {
@@ -55,5 +72,15 @@ class AuthorRepositoryTest {
         Author author = new Author();
         author.setName(name);
         return authorRepository.save(author);
+    }
+
+    private BookAndAuthor givenBookAndAuthor(Book book, Author author) {
+        BookAndAuthor bookAndAuthor = new BookAndAuthor();
+
+        bookAndAuthor.setBook(book);
+        bookAndAuthor.setAuthor(author);
+
+        return bookAndAuthorRepository.save(bookAndAuthor);
+
     }
 }
