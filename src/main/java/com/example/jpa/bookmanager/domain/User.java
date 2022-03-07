4561b487d2bf36b6268c8587b780367034ee94d2
kimseokjin <kimseokjin0324@gmail.com>
2022-03-07 14:09:22 +0900

    JPA에서 제공하는 AuditingEntityListener를 이용하여 우리가 세팅한 EntityListener를 사용하지않고 똑같은 기능 구현

--- src/main/java/com/example/jpa/bookmanager/domain/User.java
+++ src/main/java/com/example/jpa/bookmanager/domain/User.java
@@ -1,6 +1,9 @@
 package com.example.jpa.bookmanager.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,8 +14,8 @@ import java.time.LocalDateTime;
 @Data
 @Entity
 @Builder
-@EntityListeners(value = {MyEntityListener.class,UserEntityListener.class})
-public class User implements Auditable{
+@EntityListeners(value = {AuditingEntityListener.class, UserEntityListener.class})
+public class User implements Auditable {
     @Id             //PK
     @GeneratedValue //순차적으로 값을 자동으로 추가시켜주기 위해서
     private Long id;
@@ -25,7 +28,9 @@ public class User implements Auditable{
     private Gender gender;
 
     @Column(updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     //    @OneToMany(fetch = FetchType.EAGER)
