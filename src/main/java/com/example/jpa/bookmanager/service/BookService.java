4cda908242ba195d5435947573c6cf67ffbfa234
kimseokjin <kimseokjin0324@gmail.com>
2023-03-06 23:49:19 +0900

    트랜잭션 Propagation에대해서 알아보기

--- src/main/java/com/example/jpa/bookmanager/service/BookService.java
+++ src/main/java/com/example/jpa/bookmanager/service/BookService.java
@@ -6,6 +6,7 @@ import com.example.jpa.bookmanager.repository.AuthorRepository;
 import com.example.jpa.bookmanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,19 +15,35 @@ public class BookService {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final AuthorService authorService;
 
-
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void putBookAndAuthor() {
         Book book = new Book();
         book.setName("JPA 시작하기");
 
         bookRepository.save(book);
+        try {
+            authorService.putAuthor();
+        } catch (RuntimeException e) {
 
-        Author author = new Author();
-        author.setName("martin");
+        }
+//        throw new RuntimeException("오류가 발생하였습니다 transaction은 어떻게 될까요? ");
+//        Author author = new Author();
+//        author.setName("martin");
+//
+//        authorRepository.save(author);
 
-        authorRepository.save(author);
+
+    }
+
+    @Transactional
+    public void get(Long id) {
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
 
     }
 }
--- src/test/java/com/example/jpa/bookmanager/service/BookServiceTest.java
+++ src/test/java/com/example/jpa/bookmanager/service/BookServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.jpa.bookmanager.service;
 
+import com.example.jpa.bookmanager.domain.Book;
 import com.example.jpa.bookmanager.repository.AuthorRepository;
 import com.example.jpa.bookmanager.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -10,17 +11,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BookServiceTest {
 
     @Autowired
-    private  BookService bookService;
+    private BookService bookService;
     @Autowired
-    private  BookRepository bookRepository;
+    private BookRepository bookRepository;
     @Autowired
-    private  AuthorRepository authorRepository;
+    private AuthorRepository authorRepository;
 
     @Test
     void transactionTest() {
+
         bookService.putBookAndAuthor();
+
 
         System.out.println("books : " + bookRepository.findAll());
         System.out.println("authors : " + authorRepository.findAll());
+    }
+
+    @Test
+    void isolationTest() {
+        Book book = new Book();
+        book.setName("JPA강의");
+        bookRepository.save(book);
+
+        bookService.get(1L);
+
+        System.out.println(">>>>" + bookRepository.findAll());
     }
 }
