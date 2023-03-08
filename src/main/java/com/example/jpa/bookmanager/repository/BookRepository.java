2a3a8f819db81ada4020260deacb8f30fa4972db
kimseokjin <kimseokjin0324@gmail.com>
2023-03-09 01:50:17 +0900

    JPA @Query Anotation에 대해 알아보기

--- src/main/java/com/example/jpa/bookmanager/repository/BookRepository.java
+++ src/main/java/com/example/jpa/bookmanager/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package com.example.jpa.bookmanager.repository;
 
 import com.example.jpa.bookmanager.domain.Book;
+import com.example.jpa.bookmanager.repository.dto.BookNameAndCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,5 +18,17 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByCategoryIsNullAndDeletedFalse();
 
     List<Book> findByCategoryIsNullAndNameEqualsAndCreatedAtGreaterThanEqualAndUpdatedAtGreaterThanEqual(String name, LocalDateTime createdAt
-    ,LocalDateTime updatedAt);
+            , LocalDateTime updatedAt);
+
+    @Query(value = "select b from Book b where name = ?1 and createdAt >= ?2 and updatedAt >= ?3 and category is null")
+    List<Book> findByNameRecently(String name, LocalDateTime createdAt, LocalDateTime updateAt);
+
+    @Query(value = "select b from Book b" +
+            "where name = :name and createdAt >= :createdAt and updatedAt >= :updatedAt and category is null")
+    List<Book> findByNameRecently_2(
+            @Param("name") String name,
+            @Param("createdAt") LocalDateTime createdAt,
+            @Param("updatedAt") LocalDateTime updatedAt);
+
+
 }
